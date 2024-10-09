3df92b54ca1da4193b7437f9fe40083355f56485
TejasKhilare <164288299+TejasKhilare@users.noreply.github.com>
2024-10-09 09:28:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Login from "./components/users/Login";
+import Register from "./components/users/Register";
+import Index from "./components/Index";
+import Main from "./components/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index/>}/>
+          <Route path="/myspends/:userid" element={<Main/>}>
+          </Route>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
